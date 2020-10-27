6a20d5b02715d8f21b37c5d13b347d6602b48dd5
VitaliiRadaiev <hf.chelenger@gmail.com>
2020-10-27 18:32:24 +0200

    fix

--- #src/js/#marques.js
+++ #src/js/#marques.js
@@ -19,4 +19,40 @@
 			});
 		});
 	}
+
+
+	let tabs = document.querySelectorAll('.info-block__tabs-content');
+	if(tabs.length>0) {
+
+		tabs.forEach(contetn => {
+			addColumns(contetn.querySelector('.info-block__tabs-text-wrap'));
+		})
+
+
+		function addColumns(block) {
+			let arr = [...block.children];
+			let num = Math.ceil(arr.length / 3);
+			block.classList.add('block-columns-3');
+
+			for(let i = 0; i < 3; i++) {
+				let column = document.createElement('div');
+				column.className = 'column';
+
+				let innerArr = arr.slice(0, num + 1);
+
+				if(arr.length > num) {
+					arr = arr.slice(num);
+				}
+
+				if(innerArr.length) {
+					innerArr.forEach(item => {
+						column.append(item);
+					})
+				}
+
+				block.append(column);
+			}
+
+		}
+	}
 }
